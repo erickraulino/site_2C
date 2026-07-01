78e228d87913d79892f34d494607e5a236deadf1
Erick Raulino <erickraulino21@gmail.com>
2026-07-01 11:33:11 -0300

    incluido o js

---
+++ js/prog_01.js
@@ -0,0 +1,11 @@
+let resposta =  document.getElementById('resposta')
+
+function principal(){
+    let num1 = Number(document.getElementById('num1').value)
+    let num2 = Number(document.getElementById('num2').value)
+    let resultado = num1 + num2
+
+    resposta.innerHTML = ''
+    resposta.innerHTML += `a soma dos dois numeros é: ${resultado.toFixed(2)}`
+    resposta.style.fontSize = '2.rem'
+}
